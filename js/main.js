42614ba40725521b6b347784f28816f55607eb1c
leejunghyun <mygumi@cizion.com>
2016-07-17 14:43:25 +0900

    셋팅

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by leejunghyun on 16. 7. 17..
+ */
+
+var gitTest = (function () {
+    var git = {};
+
+    git.init = function () {
+        if (true) {
+            git.start();
+        }
+
+    }
+
+    git.start = function () {
+
+    }
+
+    return git;
+})
+
+gitTest().init();
